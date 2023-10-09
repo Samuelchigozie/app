6df13223bfddb895574b37b8575db84bfeb68e12
samuel chigozie <nzekwesammy@gmail.com>
2023-10-08 17:13:50 -0700

    added table list of all products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,24 @@
     <title>Products Page</title>
 </head>
 <body>
-    <h1>Products</h1>
-    <div>Hello product list</div>
+    <h1>Products List</h1>
+    <div>
+        <table border="1">
+            <th>ID</th>
+            <th>NAME</th>
+            <th>QTY</th>
+            <th>PRICE</th>
+            <th>DESCRIPTION</th>
+            @foreach($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->name}}</td>
+                <td>{{$product->qty}}</td>
+                <td>₦{{$product->price}}</td>
+                <td>{{$product->description}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
